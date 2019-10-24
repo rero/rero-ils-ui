cd8efebf90fdda0be57d8197bd5809f37c832ff9
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2019-10-24 11:58:00 +0200

    admin: Fixes new detail url parameter on brief view
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/admin/src/app/record/brief-view/circ-policies-brief-view.component.ts
+++ projects/admin/src/app/record/brief-view/circ-policies-brief-view.component.ts
@@ -21,12 +21,11 @@ import { ResultItem } from '@rero/ng-core';
 @Component({
   selector: 'admin-circ-policies-brief-view',
   template: `
-  <h5 class="mb-0 card-title">{{record.metadata.name}}</h5>
-
+  <h5 class="mb-0 card-title">{{ record.metadata.name }}</h5>
   <div class="card-text">
-  <span *ngIf="record.metadata.description">
-  {{record.metadata.description}}
-  </span>
+    <span *ngIf="record.metadata.description">
+      {{ record.metadata.description }}
+    </span>
   </div>
   `,
   styles: []
@@ -39,4 +38,6 @@ export class CircPoliciesBriefViewComponent implements ResultItem {
   @Input()
   type: string;
 
+  @Input()
+  detailUrl: string;
 }
