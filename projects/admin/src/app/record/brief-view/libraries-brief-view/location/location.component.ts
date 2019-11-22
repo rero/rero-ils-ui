7ac14c8d514d1cd75bae47c4b22327d4d9f492f3
Alicia Zangger <alicia.zangger@rero.ch>
2019-11-22 14:05:13 +0100

    locations: implement detail view
    * Implements detail view for locations.
    * Improves libraries brief view.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

---
+++ projects/admin/src/app/record/brief-view/libraries-brief-view/location/location.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { RecordUiService } from '@rero/ng-core';
+import { RecordPermissionMessageService } from 'projects/admin/src/app/service/record-permission-message.service';
+import { EventEmitter } from 'events';
+
+@Component({
+  selector: 'admin-location',
+  templateUrl: './location.component.html'
+})
+export class LocationComponent {
+
+  /** The location whose details are displayed */
+  @Input() location: any;
+
+  /** Delete location event emitter */
+  @Output() deleteLocation = new EventEmitter();
+
+  /** Constructor */
+  constructor(
+    private recordUiService: RecordUiService,
+    private recordPermissionMessage: RecordPermissionMessageService
+  ) { }
+
+  /**
+   * Delete the location
+   * @param locationPid - location PID
+   */
+  delete(locationPid: string) {
+    this.recordUiService.deleteRecord('items', locationPid).subscribe((success: boolean) => {
+      if (success) {
+        this.deleteLocation.emit(locationPid);
+      }
+    });
+  }
+
+  /**
+   * Show a message after deletion action
+   * @param location : object - location concerned by the delete action
+   */
+  public showDeleteMessage(location: object) {
+    const message = this.recordPermissionMessage.generateMessage(location);
+    this.recordUiService.showDeleteMessage(message);
+  }
+
+}
