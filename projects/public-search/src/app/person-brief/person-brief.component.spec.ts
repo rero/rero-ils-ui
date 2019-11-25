ac65680ca09eac057791f2ab976d9edc1ffc6de4
Alicia Zangger <alicia.zangger@rero.ch>
2019-11-25 12:18:25 +0100

    public-search: repare detail links
    * Repares documents and persons detail links.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

--- projects/public-search/src/app/person-brief/person-brief.component.spec.ts
+++ projects/public-search/src/app/person-brief/person-brief.component.spec.ts
@@ -21,6 +21,11 @@ import { MefTitlePipe } from './../pipes/mef-title.pipe';
 import { BirthDatePipe } from './../pipes/birth-date.pipe';
 import { BioInformationsPipe } from './../pipes/bio-informations.pipe';
 import { PersonBriefComponent } from './person-brief.component';
+import { AppRoutingModule } from 'projects/admin/src/app/app-routing.module';
+import { FrontpageComponent } from 'projects/admin/src/app/frontpage/frontpage.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RecordModule } from '@rero/ng-core';
 
 describe('PersonBriefComponent', () => {
   let component: PersonBriefComponent;
@@ -28,7 +33,20 @@ describe('PersonBriefComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PersonBriefComponent, MefTitlePipe, BirthDatePipe, BioInformationsPipe ]
+      declarations: [
+        PersonBriefComponent,
+        MefTitlePipe,
+        BirthDatePipe,
+        BioInformationsPipe,
+        FrontpageComponent
+      ],
+      imports:
+      [
+        RecordModule,
+        HttpClientModule,
+        TranslateModule.forRoot(),
+        AppRoutingModule
+      ]
     })
     .compileComponents();
   }));
