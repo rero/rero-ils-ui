14adf2f84be4bf5065281f666d0092755e06c8f5
Lauren-D <laurent.dubois@itld-solutions.be>
2020-01-28 13:10:20 +0100

    acquisition: link order line to a document
    * Implements link between order line and document.
    * Fixes div alignment for order lines table in order detail view.
    * Adds filter by user current library on acquisition account field in editor.
    
    Co-Authored-by: Laurent Dubois <laurent.dubois@itld-solutions.be>

--- projects/admin/src/app/routes/acquisition-order-lines-route.ts
+++ projects/admin/src/app/routes/acquisition-order-lines-route.ts
@@ -14,12 +14,15 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { RouteInterface, DetailComponent, EditorComponent, extractIdOnRef } from '@rero/ng-core';
+import { DetailComponent, EditorComponent, extractIdOnRef, RecordService, RouteInterface} from '@rero/ng-core';
 import { BaseRoute } from './Base-route';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { JSONSchema7 } from 'json-schema';
 import {
   AcquisitionOrderLineDetailViewComponent
 } from '../record/detail-view/acquisition-order-line-detail-view/acquisition-order-line-detail-view.component';
 import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export class AcquisitionOrderLinesRoute extends BaseRoute implements RouteInterface {
 
@@ -60,10 +63,54 @@ export class AcquisitionOrderLinesRoute extends BaseRoute implements RouteInterf
             redirectUrl: (record: any) => {
               const acqOrderPid = extractIdOnRef(record.metadata.acq_order.$ref);
               return of(`/records/acq_orders/detail/${acqOrderPid}`);
-            }
+            },
+            formFieldMap: (field: FormlyFieldConfig, jsonSchema: JSONSchema7): FormlyFieldConfig => {
+              return this.populateAcquisitionAccountsByCurrentUserLibrary(
+                field, jsonSchema
+              );
+            },
           }
         ]
       }
     };
+  }
+
+  /**
+   * Populate select menu with acquisition accounts of current user library
+   * @param field - FormlyFieldConfig
+   * @param jsonSchema - JSONSchema7
+   * @return FormlyFieldConfig
+   */
+  private populateAcquisitionAccountsByCurrentUserLibrary(
+    field: FormlyFieldConfig,
+    jsonSchema: JSONSchema7): FormlyFieldConfig {
+    const formOptions = jsonSchema.form;
+    if (formOptions && formOptions.fieldMap === 'acq_account') {
+      field.type = 'select';
+      const user = this._routeToolService.userService.getCurrentUser();
+      const recordService = this._routeToolService.recordService;
+      const apiService = this._routeToolService.apiService;
+      const libraryPid = user.currentLibrary;
+      const query = `library.pid:${libraryPid}`;
+      field.templateOptions.options = recordService.getRecords(
+        'acq_accounts',
+        query, 1,
+        RecordService.MAX_REST_RESULTS_SIZE
+      ).pipe(
+        map(result => result.hits.total === 0 ? [] : result.hits.hits),
+        map(hits => {
+          return hits.map((hit: any) => {
+            return {
+              label: hit.metadata.name,
+              value: apiService.getRefEndpoint(
+                'acq_accounts',
+                hit.metadata.pid
+              )
+            };
+          });
+        })
+      );
+    }
+    return field;
   }
 }
