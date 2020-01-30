00cd64e2c63aa584c3f7ee49c397b9aa6cace063
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2020-01-30 12:48:14 +0100

    person: implement documents on detail view
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/admin/src/app/record/detail-view/person-detail-view/person-detail-view.component.ts
+++ projects/admin/src/app/record/detail-view/person-detail-view/person-detail-view.component.ts
@@ -14,22 +14,48 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DetailRecord } from '@rero/ng-core/lib/record/detail/view/detail-record';
 import { Observable } from 'rxjs';
+import { RecordService } from '@rero/ng-core';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'admin-person-detail-view',
   templateUrl: './person-detail-view.component.html',
   styles: []
 })
-export class PersonDetailViewComponent implements DetailRecord {
+export class PersonDetailViewComponent implements DetailRecord, OnInit {
 
-    /** Observable resolving record data */
-    record$: Observable<any>;
+  /** Observable resolving record data */
+  record$: Observable<any>;
 
-    /** Resource type */
-    type: string;
+  /** Resource type */
+  type: string;
 
-  constructor() { }
+  /** Documents for current person */
+  documents$: Observable<Array<any>>;
+
+  /**
+   * Constructor
+   * @param _recordService - RecordService
+   */
+  constructor(
+    private _recordService: RecordService
+  ) { }
+
+  /**
+   * Init
+   */
+  ngOnInit() {
+    this.documents$ = this.record$.pipe(
+      switchMap(record => {
+        const personPid = record.metadata.pid;
+        const query = `authors.pid:${personPid}`;
+        return this._recordService.getRecords(
+        'documents', query, 1, RecordService.MAX_REST_RESULTS_SIZE
+      ); }),
+      map(hits => hits.hits.total === 0 ? [] : hits.hits.hits)
+    );
+  }
 }
