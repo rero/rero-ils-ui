5ebbdaf14acff8f3917bc602d32d9cdc2ce2d7db
Alicia Zangger <alicia.zangger@rero.ch>
2020-04-03 13:22:34 +0200

    request: implement request list edition in item detail view
    * Adds 'Add' button at the top of request list to access 'add request
    form'.
    * Implements request deletion with confirmation pop-up.
    * Implements request pickup location edition.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

--- projects/admin/src/app/service/loan.service.ts
+++ projects/admin/src/app/service/loan.service.ts
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecordService } from '@rero/ng-core';
 import { map } from 'rxjs/operators';
@@ -23,10 +24,16 @@ import { map } from 'rxjs/operators';
 })
 export class LoanService {
 
+  /**
+   * Status of a borrow loan
+   */
   statusBorrow = {
     ON_LOAN: 'ITEM_ON_LOAN'
   };
 
+  /**
+   * Statuses of a request loan
+   */
   statusRequest = {
     AT_DESK: 'ITEM_AT_DESK',
     PENDING: 'PENDING',
@@ -35,9 +42,12 @@ export class LoanService {
 
   /**
    * Constructor
-   * @param recordService RecordService
+   * @param _recordService RecordService
    */
-  constructor(private recordService: RecordService) { }
+  constructor(
+    private _recordService: RecordService,
+    private _http: HttpClient
+  ) { }
 
   /**
    * Return the number of request(s) on item
@@ -47,7 +57,7 @@ export class LoanService {
   numberOfRequests$(itemPid: string) {
     const states = Object.values(this.statusRequest).join(' OR state:');
     const query = `item_pid:${itemPid} AND (state:${states})`;
-    return this.recordService.getRecords('loans', query, 1, 1).pipe(
+    return this._recordService.getRecords('loans', query, 1, 1).pipe(
       map(result => result.hits.total)
     );
   }
@@ -81,6 +91,46 @@ export class LoanService {
   }
 
   /**
+   * Cancel a loan related to an item
+   * @param itemPid: item pid related to the loan to cancel
+   * @param loanPid: loan to cancel pid
+   * @param transactionLibraryPid: transaction library pid
+   * @return item data
+   */
+  cancelLoan(itemPid: string, loanPid: string, transactionLibraryPid: string) {
+    const url = '/api/item/cancel';
+    return this._http.post<any>(url, {
+      item_pid: itemPid,
+      pid: loanPid,
+      transaction_library_pid: transactionLibraryPid
+    }).pipe(
+    map(data => {
+      const itemData = data.metadata;
+      itemData.loan = data.action_applied.cancel;
+      return itemData;
+    }));
+  }
+
+  /**
+   * Update the pickup location of a loan
+   * @param loan: loan to update
+   * @param pickupLocationPid: pickup location pid to update
+   * @return item data
+   */
+  updateLoanPickupLocation(loanPid: string, pickupLocationPid: string, itemPid: string) {
+    const url = '/api/item/update_loan_pickup_location';
+    return this._http.post<any>(url, {
+      loan_pid: loanPid,
+      pickup_location_pid: pickupLocationPid,
+      item_pid: itemPid
+    }).pipe(
+    map(loanData => {
+      return loanData;
+    }));
+  }
+
+  /**
+   * Loans
    * @param itemPid Item Pid
    * @return Observable
    */
@@ -92,6 +142,6 @@ export class LoanService {
     ];
     const states = statuses.join(' OR state:');
     const query = `item_pid:${itemPid} AND (state:${states})`;
-    return this.recordService.getRecords('loans', query, 1, 100);
+    return this._recordService.getRecords('loans', query, 1, 100);
   }
 }
