3bd21f148536ae5555301de970961e24cd591cf7
Alicia Zangger <alicia.zangger@rero.ch>
2020-06-22 15:10:26 +0200

    document: adapt display of field "notes"
    * Creates a pipe to display the field "notes".
    * Corrects detail view accordingly.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

---
+++ projects/admin/src/app/pipe/notes-format.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NotesFormatPipe } from './notes-format.pipe';
+
+describe('NotesFormatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NotesFormatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
