75640ba597fe3933d40bd96119bd19b2e2b03469
Lauren-D <laurent.dubois@itld-solutions.be>
2020-07-20 19:07:21 +0200

    project: downgrade ngx-spinner version
    * Downgrades the ngx-spinner version for compatibility with Angular 8.
    * Updates two HTML templates to use spinner on ng-core components
      because the migration to ngx-spinner removed the old spinner.
    * Uses ng-core v0.6.0.
    
    Co-Authored-by: Lauren-D <laurent.dubois@itld-solutions.be>

--- projects/public-search/src/app/app.component.spec.ts
+++ projects/public-search/src/app/app.component.spec.ts
@@ -17,13 +17,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RecordModule } from '@rero/ng-core';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        TranslateModule.forRoot({}),
+        RecordModule
       ],
       declarations: [
         AppComponent
