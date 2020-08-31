53982665aca0f7b652e47fcbb818f1dabea2517c
Alicia Zangger <alicia.zangger@rero.ch>
2020-08-31 10:19:12 +0200

    templates: create brief and detail views
    * Creates brief and detail views for templates resources.
    * Removes the possibility to add a template from the brief view, as it
    has to be done from the concerned resource.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

--- projects/admin/src/app/routes/templates-route.ts
+++ projects/admin/src/app/routes/templates-route.ts
@@ -17,8 +17,8 @@
  */
 import { DetailComponent, EditorComponent, RecordSearchComponent, RouteInterface } from '@rero/ng-core';
 import { CanUpdateGuard } from '../guard/can-update.guard';
-import { LibrariesBriefViewComponent } from '../record/brief-view/libraries-brief-view.component';
-import { LibraryDetailViewComponent } from '../record/detail-view/library-detail-view/library-detail-view.component';
+import { TemplatesBriefViewComponent } from '../record/brief-view/templates-brief-view.component';
+import { TemplateDetailViewComponent } from '../record/detail-view/template-detail-view/template-detail-view.component';
 import { BaseRoute } from './base-route';
 
 export class TemplatesRoute extends BaseRoute implements RouteInterface {
@@ -27,7 +27,7 @@ export class TemplatesRoute extends BaseRoute implements RouteInterface {
   readonly name = 'templates';
 
   /** Record type */
-  readonly recordType = 'libraries';
+  readonly recordType = 'templates';
 
   /**
    * Get Configuration
@@ -46,12 +46,17 @@ export class TemplatesRoute extends BaseRoute implements RouteInterface {
         types: [
           {
             key: this.name,
-            label: 'Libraries',
-            component: LibrariesBriefViewComponent,  // TODO : To be changed
-            detailComponent: LibraryDetailViewComponent,  // TODO : To be changed
-            canAdd: () => this._routeToolService.canSystemLibrarian(),
+            label: 'Templates',
+            component: TemplatesBriefViewComponent,
+            detailComponent: TemplateDetailViewComponent,
+            canAdd: () => this._routeToolService.canNot(),
             canUpdate: (record: any) => this._routeToolService.canUpdate(record, this.recordType),
             canDelete: (record: any) => this._routeToolService.canDelete(record, this.recordType),
+            aggregationsOrder: [
+              'template_type',
+              'visibility'
+            ],
+            aggregationsExpand: ['template_type'],
             // use simple query for UI search
             preFilters: {
               simple: 1
