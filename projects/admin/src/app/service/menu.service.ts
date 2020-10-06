8874a030c131f90c6a0daddb0cbae3b06ad94387
Alicia Zangger <alicia.zangger@rero.ch>
2020-10-06 10:16:40 +0200

    patron: rename as user
    * Replaces 'patron' with 'user' in menu and users list.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

--- projects/admin/src/app/service/menu.service.ts
+++ projects/admin/src/app/service/menu.service.ts
@@ -44,10 +44,10 @@ export class MenuService {
           iconCssClass: 'fa fa-shopping-basket',
           id: 'requests-menu'
         }, {
-          name: this._translateService.instant('Patrons'),
+          name: this._translateService.instant('Users'),
           routerLink: '/records/patrons',
           iconCssClass: 'fa fa-users',
-          id: 'patrons-menu'
+          id: 'users-menu'
         }, {
           name: this._translateService.instant('Collections'),
           routerLink: '/records/collections',
