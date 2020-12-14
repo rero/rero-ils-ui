90cb02b02f332c2515d25a7e3be06dce8d18d64f
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2020-12-14 10:18:17 +0100

    search: fix ILL request search results errors
    * Fixes errors when the search results of the ILL requests are displayed.
    
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- projects/admin/src/app/routes/ill-requests-route.ts
+++ projects/admin/src/app/routes/ill-requests-route.ts
@@ -15,11 +15,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { FormlyFieldConfig } from '@ngx-formly/core';
-import { DetailComponent, EditorComponent, RecordSearchComponent, RouteInterface } from '@rero/ng-core';
-import { JSONSchema7 } from 'json-schema';
+import { DetailComponent, EditorComponent, RecordSearchPageComponent, RouteInterface } from '@rero/ng-core';
 import { CanUpdateGuard } from '../guard/can-update.guard';
-import { RoleGuard } from '../guard/role.guard';
 import { IllRequestsBriefViewComponent } from '../record/brief-view/ill-requests-brief-view/ill-requests-brief-view.component';
 import { IllRequestDetailViewComponent } from '../record/detail-view/ill-request-detail-view/ill-request-detail-view.component';
 import { BaseRoute } from './base-route';
@@ -40,7 +37,7 @@ export class IllRequestsRoute extends BaseRoute implements RouteInterface {
     return {
       matcher: (url: any) => this.routeMatcher(url, this.name),
       children: [
-        { path: '', component: RecordSearchComponent },
+        { path: '', component: RecordSearchPageComponent },
         { path: 'detail/:pid', component: DetailComponent },
         { path: 'edit/:pid', component: EditorComponent, canActivate: [CanUpdateGuard] },
         { path: 'new', component: EditorComponent }
