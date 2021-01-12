2e1209633c5174d372275a5fa7d1854aa12f716c
Renaud Michotte <renaud.michotte@gmail.com>
2021-01-12 15:28:20 +0100

    search: fix encoding parameters problem
    Some characters for the query parameters in the URL are escaped by
    default by the query request encoder. This commit make the request more
    robust.
    
    Closes rero/rero-ils#1237.
    
    Co-Authored-By : Renaud Michotte <renaud.michotte@gmail.com>
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

---
+++ projects/public-search/src/app/interceptor/custom-request.interceptor.ts
@@ -0,0 +1,70 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2019 RERO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpParams,
+  HttpEvent,
+  HttpUrlEncodingCodec
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+class SafeUrlParamsEncoder extends HttpUrlEncodingCodec {
+
+  /**
+   * Encodes a key name for a URL parameter or query-string.
+   * @param key The key name.
+   * @returns The encoded key name.
+   */
+  encodeKey(key: string): string {
+    return encodeURIComponent(key);
+  }
+
+  /**
+   * Encodes the value of a URL parameter or query-string.
+   * @param value The value.
+   * @returns The encoded value.
+   */
+  encodeValue(value: string): string {
+    return encodeURIComponent(value);
+  }
+}
+
+
+@Injectable()
+export class CustomRequestInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    // encode URL parameters
+    // angular does not do it by default,
+    // see: https://github.com/angular/angular/issues/18261 for more details
+    const params = new HttpParams({ encoder: new SafeUrlParamsEncoder(), fromString: req.params.toString() });
+    const authReq = req.clone({
+      // Prevent caching in IE, in particular IE11.
+      // See: https://support.microsoft.com/en-us/help/234067/how-to-prevent-caching-in-internet-explorer
+      setHeaders: {
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache'
+      }
+    });
+    return next.handle(authReq.clone({ params }));
+  }
+}
