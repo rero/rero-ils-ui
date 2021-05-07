0992d90be0bab6aaab8c8b4bc8e7ca8dc5f43d78
Renaud Michotte <renaud.michotte@gmail.com>
2021-05-07 10:44:32 +0200

    Issue: update links on late issue brief view
    * For a late issue brief view, the main ink should point to the holdings
      detail view instead of item detail view. The item detail link is now
      placed on the barcode.
    * Adds a link to point on holding parent detail view on the item detail
      view if the item is an issue.
    * Closes rero/rero-ils#1808.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/record/brief-view/issues-brief-view/issues-brief-view.component.ts
+++ projects/admin/src/app/record/brief-view/issues-brief-view/issues-brief-view.component.ts
@@ -15,29 +15,36 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ResultItem } from '@rero/ng-core';
 import { IssueItemStatus } from '@rero/shared';
 
 @Component({
   selector: 'admin-inventory-brief-view',
-  templateUrl: './issues-brief-view.component.html'
+  templateUrl: './issues-brief-view.component.html',
 })
-export class IssuesBriefViewComponent implements ResultItem {
+export class IssuesBriefViewComponent implements ResultItem, OnInit {
 
+  /** Record */
+  @Input() record: any;
+  /** Type of record */
+  @Input() type: string;
+  /** Detail Url */
+  @Input() detailUrl: { link: string, external: boolean };
+
+  /** prent holding url */
+  parentUrl: { link: string, external: boolean };
   /** reference to IssueItemStatus */
   issueItemStatus = IssueItemStatus;
 
-  /** Record */
-  @Input()
-  record: any;
-
-  /** Type of record */
-  @Input()
-  type: string;
-
-  /** Detail Url */
-  @Input()
-  detailUrl: { link: string, external: boolean };
+  /** OnInit hook */
+  ngOnInit() {
+    if (this.record) {
+      this.parentUrl = {
+        link: `/records/holdings/detail/${this.record.metadata.holding.pid}`,
+        external: false
+      };
+    }
+  }
 
 }
