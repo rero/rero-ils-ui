774e36ddf9e68aa7f88ba6247cfed09382d47e0f
Renaud Michotte <renaud.michotte@gmail.com>
2021-05-20 10:04:19 +0200

    acquisitions: create module
    * Creates a `acquisition` module.
    * Moves legacy components/services related to acquisition features into
      this new module.
    * Creates pages to list acquisition accounts using a tree view to
      respect accounts hierarchy.
    * Creates form to transfer funds between two accounts.
    * Corrects linting warning problems.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

---
+++ projects/admin/src/app/acquisition/classes/budget.ts
@@ -0,0 +1,41 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2021 RERO
+ * Copyright (C) 2021 UCLouvain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* tslint:disable */
+// required as json properties is not lowerCamelCase
+
+import { ObjectReference } from '../../classes/core';
+
+/** Wrapping class to describe an AcqAccount */
+export class AcqBudget {
+  $schema: string = null;
+  pid: string = null;
+  name: string = '';
+  start_date: string = '';
+  end_date: string = '';
+  is_active: boolean = false;
+  organisation: ObjectReference;
+
+  /**
+   * Constructor
+   * @param obj - the JSON parsed object to load.
+   */
+  constructor(obj?: any){
+    Object.assign(this, obj);
+  }
+}
---
+++ projects/admin/src/app/classes/core.ts
@@ -0,0 +1,26 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2021 RERO
+ * Copyright (C) 2021 UCLouvain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* tslint:disable */
+// required as json properties is not lowerCamelCase
+
+/** Interface to describe a reference between two object */
+export interface ObjectReference {
+  pid: string;
+  type?: string;
+}
