ef3504cbd191d90d81bcac8dcaf5169fd2f04671
Renaud Michotte <renaud.michotte@gmail.com>
2021-09-06 10:15:31 +0200

    acquisition: improve account/orders display.
    * Improves account detail view.
    * Improves orders detail view.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/acquisition/components/order/order-detail-view/order-detail-view.component.ts
+++ projects/admin/src/app/acquisition/components/order/order-detail-view/order-detail-view.component.ts
@@ -14,75 +14,111 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RecordService, RecordUiService } from '@rero/ng-core';
 import { DetailRecord } from '@rero/ng-core/lib/record/detail/view/detail-record';
 import { Record } from '@rero/ng-core/lib/record/record';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AcqOrderApiService } from '../../../api/acq-order-api.service';
+import { AcqNoteType, AcqOrder, AcqOrderLine } from '../../../classes/order';
 
 @Component({
   selector: 'admin-acquisition-order-detail-view',
   templateUrl: './order-detail-view.component.html',
   styleUrls: ['./order-detail-view.component.scss']
 })
-export class OrderDetailViewComponent implements OnInit, DetailRecord {
+export class OrderDetailViewComponent implements OnInit, OnDestroy, DetailRecord {
 
   // COMPONENT ATTRIBUTES =====================================================
   /** Observable resolving record data */
   record$: Observable<any>;
   /** Resource type */
   type: string;
-  /** Acquisition Order total amount observable */
-  totalAmount$: Observable<any>;
+  /** the order corresponding to the record */
+  order: AcqOrder;
+  /** Is order notes are collapsed */
+  notesCollapsed = true;
   /** Acquisition order Line observable */
   orderLines$: Observable<Array<any>>;
 
+  /** all component subscription */
+  private _subscriptions = new Subscription();
+
   // CONSTRUCTOR & HOOKS ======================================================
   /** Constructor
+   * @param _acqOrderApiService - AcqOrderApiService,
    * @param _recordService - RecordService
    * @param _recordUiService - RecordUiService
+   * @param _scroller - ViewportScroller
    */
   constructor(
+    private _acqOrderApiService: AcqOrderApiService,
     private _recordService: RecordService,
     private _recordUiService: RecordUiService,
+    private _scroller: ViewportScroller
   ) { }
 
   /** OnInit hook */
   ngOnInit() {
     // init total amount
-    this.totalAmount$ = this.record$.pipe(
-      map(record => record.metadata.total_amount)
-    );
-    // retrieve all order lines linked
-    this.orderLines$ = this.record$.pipe(
-      switchMap(record => {
-        const query = `acq_order.pid:${record.metadata.pid}`;
-        return this._recordService.getRecords(
+    this.record$.subscribe(
+      (record: any) => {
+        this.order = new AcqOrder(record.metadata);
+        const query = `acq_order.pid:${this.order.pid}`;
+        this.orderLines$ = this._recordService.getRecords(
           'acq_order_lines', query, 1, RecordService.MAX_REST_RESULTS_SIZE,
           undefined, undefined, undefined, 'pid'
         ).pipe(
-          map((result: Record) => result.hits.hits)
+          map((result: Record) => result.hits.hits),
+          map((hits: Array<any>) => hits.map(hit => new AcqOrderLine(hit.metadata)))
         );
-      })
+      }
+    );
+    // Subscription when an order line is deleted
+    this._subscriptions.add(
+      this._acqOrderApiService.deletedOrderLineSubject$.subscribe((orderLine: AcqOrderLine) => this._deleteOrderLine(orderLine))
     );
   }
 
-  // COMPONENT FUNCTIONS =======================================================
-  /**
-   * Delete order line event
-   * @param orderLinePid - Order PID
-   */
-  deleteOrderLine(orderLinePid: string) {
-    this._recordUiService.deleteRecord('acq_order_lines', orderLinePid).subscribe((success: any) => {
-      if (success) {
-        this.orderLines$ = this.orderLines$.pipe(
-          map(orderLines =>
-            orderLines.filter((orderLine: any) => orderLinePid !== orderLine.metadata.pid)
-          )
-        );
-        this.totalAmount$ = this.record$.pipe(map(record => record.metadata.total_amount));
-      }
-    });
+  /** OnDestroy hook */
+  ngOnDestroy() {
+    this._subscriptions.unsubscribe();
   }
+
+  // COMPONENT FUNCTIONS =======================================================
+
+  /** Scroll to an anchor
+   *  @param e - the fired event
+   *  @param anchorId - the anchor ID
+   */
+  scrollTo(e: Event, anchorId: string): void {
+    e.preventDefault();
+    this._scroller.scrollToAnchor(anchorId);
+  }
+
+  /** Get the badge color to use for a note type
+   *  @param noteType - the note type
+   */
+  getBadgeColor(noteType: AcqNoteType): string {
+    switch (noteType) {
+      case AcqNoteType.STAFF_NOTE: return 'badge-info';
+      case AcqNoteType.VENDOR_NOTE: return 'badge-warning';
+      default: return 'badge-secondary';
+    }
+  }
+
+  // COMPONENT PRIVATE FUNCTIONS ==============================================
+  /**
+   * Delete an order line
+   * @param orderLine - The order line to remove
+   */
+  private _deleteOrderLine(orderLine: AcqOrderLine) {
+    this.order.total_amount -= orderLine.total_amount;
+    this.orderLines$ = this.orderLines$.pipe(
+      map(orderLines => orderLines.filter((line: any) => line.pid !== orderLine.pid))
+    );
+  }
+
 }
