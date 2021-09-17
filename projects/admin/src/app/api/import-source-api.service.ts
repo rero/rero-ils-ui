c14f64ad4358fa7d17096dd4d8a5a1866cbdd881
Benoit Erken <erken.benoit@gmail.com>
2021-09-17 15:16:11 +0200

    import: add new sources to import from the web
    * Adds new external sources possibilities to load document based on
      backend server configuration.
    * Improves method to detect duplicate records before loading document editor
    
    Co-Authored-by : Benoit Erken <erken.benoit@gmail.com>
    Co-Authored-by : Renaud Michotte <renaud.michotte@gmail.com>

---
+++ projects/admin/src/app/api/import-source-api.service.ts
@@ -0,0 +1,48 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2021 UCLouvain
+ * Copyright (C) 2021 RERO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ExternalSourceSetting } from '../classes/external-source';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImportSourceApiService {
+
+  /**
+   * Constructor
+   * @param _httpClient - HttpClient
+   */
+  constructor(
+    private _httpClient: HttpClient
+  ) { }
+
+  /**
+   * Get the configuration entrypoint for external sources
+   * @return: an Observable on external sources list
+   */
+  getSources(): Observable<Array<ExternalSourceSetting>> {
+    return this._httpClient.get('/api/imports/config/').pipe(
+      map((sources: [any]) => sources.map(data => new ExternalSourceSetting(data)))
+    );
+  }
+
+}
---
+++ projects/admin/src/app/classes/external-source.ts
@@ -0,0 +1,41 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2021 RERO
+ * Copyright (C) 2021 UCLouvain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* tslint:disable */
+// required as json properties is not lowerCamelCase
+
+export class ExternalSourceSetting {
+  key: string;
+  label: string = "";
+  weight?: number = 100;
+  endpoint?: string;
+
+  constructor(obj?: any) {
+    Object.assign(this, obj)
+  }
+
+  /**
+   * Get the import url to use
+   * @return: the endpoint url to use to import external sources
+   */
+  getImportEndpoint(): string {
+    return (this.endpoint != undefined)
+      ? this.endpoint
+      : `import_${this.key}`;
+  }
+}
