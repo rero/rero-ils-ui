b616d94e954a9f379710ab7b4543c7c1c275c0e9
Renaud Michotte <renaud.michotte@gmail.com>
2021-10-08 12:23:55 +0200

    patron: improve patron detail view
    * Improves patron detail view.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/record/detail-view/patron-detail-view/patron-detail-view.component.ts
+++ projects/admin/src/app/record/detail-view/patron-detail-view/patron-detail-view.component.ts
@@ -21,35 +21,74 @@ import { DetailRecord } from '@rero/ng-core/lib/record/detail/view/detail-record
 import { Observable, Subscription } from 'rxjs';
 import { OperationLogsService } from '../../../service/operation-logs.service';
 
+interface PatronPhone {
+  value: string;
+  type?: string;
+  weight: number;
+}
+
 @Component({
   selector: 'admin-patron-detail-view',
-  templateUrl: './patron-detail-view.component.html'
+  templateUrl: './patron-detail-view.component.html',
+  styleUrls: ['./patron-detail-view.component.scss']
 })
 export class PatronDetailViewComponent implements OnInit, DetailRecord, OnDestroy {
 
+  // COMPONENT ATTRIBUTES =====================================================
   /** Data from patron we received */
   record$: Observable<any>;
-
   /** Current displayed/used patron */
   patron: any;
-
   /** record type */
   type: string;
+  /** Load operation logs on show */
+  showOperationLogs = false;
+  /** collapsed sections */
+  sectionCollapsed = {
+    user: false,
+    librarian: true,
+    patron: true,
+    notes: false
+  };
 
   /** Subscription to (un)follow the record$ Observable */
   private _subscription$ = new Subscription();
+  /** mapping to define role badge color */
+  private _rolesBadgeMappings = {
+    system_librarian: 'badge-danger',
+    librarian: 'badge-warning'
+  };
 
-  /** Load operation logs on show */
-  showOperationLogs = false;
-
-  /**
-   * Is operation log enabled
-   * @return boolean
-   */
+  // GETTER AND SETTER ========================================================
+  /** Is operation log enabled */
   get isEnabledOperationLog(): boolean {
     return this._operationLogsService.isLogVisible('patrons');
   }
 
+  /** Get all phones related to the patron */
+  get phones(): Array<PatronPhone> {
+    const data: Array<PatronPhone> = [];
+    if (this.patron.mobile_phone) {
+      data.push({value: this.patron.mobile_phone, type: 'Mobile', weight: 10});
+    }
+    if (this.patron.home_phone) {
+      data.push({value: this.patron.home_phone, type: 'Home', weight: 7});
+    }
+    if (this.patron.business_phone) {
+      data.push({value: this.patron.business_phone, type: 'Business', weight: 7});
+    }
+    if (this.patron.other_phone) {
+      data.push({value: this.patron.other_phone, type: 'Other', weight: -1});
+    }
+    return data.sort((a, b) => b.weight - a.weight);
+  }
+
+  /** Check if the patron has the librarian role */
+  get isLibrarian(): boolean {
+    return this.patron && this.patron.roles && this.patron.roles.includes('librarian');
+  }
+
+  // CONSTRUCTOR & HOOKS ======================================================
   /**
    * Constructor.
    * @param _sanitizer - DomSanitizer, to render html.
@@ -60,39 +99,39 @@ export class PatronDetailViewComponent implements OnInit, DetailRecord, OnDestro
     private _operationLogsService: OperationLogsService
   ) {}
 
-  /**
-   * Current patron initialization.
-   */
+  /** OnInit hook */
   ngOnInit() {
-    this._subscription$ = this.record$.subscribe(record => {
-      this.patron = record.metadata;
-    });
+    this._subscription$ = this.record$.subscribe(record => this.patron = record.metadata);
   }
 
-  /**
-   * Unsubscribe observable when destroying the PatronDetailViewComponent
-   */
+  /** OnDestroy hook */
   ngOnDestroy() {
     this._subscription$.unsubscribe();
   }
 
+  // COMPONENTS FUNCTIONS =====================================================
+
   /**
-   * Get the patron notes.
-   *
-   * It replace a new line to the corresponding html code.
-   * Allows to render html.
+   * Get the color badge to apply for a specific role
+   * @param role: the role to check.
+   * @return the bootstrap badge class to use for this role.
    */
-  public get notes(): Array<{type: string, content: SafeHtml}> {
-    if (!this.patron.notes || this.patron.notes.length < 1) {
-      return [];
-    }
-    return this.patron.notes.map((note: any) => {
-      return {
-        type: note.type,
-        content: this._sanitizer.bypassSecurityTrustHtml(
-          note.content.replace('\n', '<br>'))
-      };
-    });
+  getRoleBadgeColor(role: string): string {
+    return (role in this._rolesBadgeMappings)
+      ? this._rolesBadgeMappings[role]
+      : 'badge-light';
   }
+
+  /** Get the badge color to use for a note type
+   *  @param noteType - the note type
+   */
+  getNoteBadgeColor(noteType: string): string {
+    switch (noteType) {
+      case 'public_note': return 'badge-info';
+      case 'staff_note': return 'badge-warning';
+      default: return 'badge-secondary';
+    }
+  }
+
 
 }
