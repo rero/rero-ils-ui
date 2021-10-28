7a143dcf6373df788469003877276489e4076a57
Renaud Michotte <renaud.michotte@gmail.com>
2021-10-28 17:07:41 +0200

    acquisitions: fix permissions problems
    Limit access to update actions (add/edit/delete) if the current user
    isn't connected with the owning resource library.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/acquisition/components/order/order-detail-view/order-line/order-line.component.ts
+++ projects/admin/src/app/acquisition/components/order/order-detail-view/order-line/order-line.component.ts
@@ -15,12 +15,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RecordService } from '@rero/ng-core';
 import { RecordPermissionService } from 'projects/admin/src/app/service/record-permission.service';
 import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RecordPermissions } from 'projects/admin/src/app/classes/permissions';
+import { CurrentLibraryPermissionValidator } from 'projects/admin/src/app/utils/permissions';
 import { AcqOrderApiService } from '../../../../api/acq-order-api.service';
 import { AcqNote, AcqNoteType, AcqOrderLine, AcqOrderLineStatus } from '../../../../classes/order';
+
 
 @Component({
   selector: 'admin-order-line',
@@ -36,7 +40,7 @@ export class OrderLineComponent implements OnInit {
   @Input() order: any;
 
   /** order line permission */
-  permissions: any;
+  permissions: RecordPermissions;
   /** order line relate account */
   account: any;
   /** Is the line is collapsed */
@@ -52,22 +56,29 @@ export class OrderLineComponent implements OnInit {
   get deleteInfoMessage(): string {
     return this._recordPermissionService.generateDeleteMessage(this.permissions.delete.reasons);
   }
+  get editInfoMessage(): string {
+    return this._recordPermissionService.generateTooltipMessage(this.permissions.update.reasons, 'update');
+  }
 
   // CONSTRUCTOR & HOOKS ======================================================
   /** Constructor
    * @param _recordPermissionService - RecordPermissionService
    * @param _recordService - RecordService
    * @param _acqOrderApiService - AcqOrderApiService
+   * @param _permissionValidator - CurrentLibraryPermissionValidator
    */
   constructor(
     private _recordPermissionService: RecordPermissionService,
     private _recordService: RecordService,
     private _acqOrderApiService: AcqOrderApiService,
+    private _permissionValidator: CurrentLibraryPermissionValidator
   ) { }
 
   /** OnInit hook */
   ngOnInit() {
-    const permissions$ = this._recordPermissionService.getPermission('acq_order_lines', this.orderLine.pid);
+    const permissions$ = this._recordPermissionService.getPermission('acq_order_lines', this.orderLine.pid).pipe(
+      map((permissions) => this._permissionValidator.validate(permissions, this.order.library.pid))
+    );
     const account$ = this._recordService.getRecord('acq_accounts', this.orderLine.acq_account.pid);
     forkJoin([permissions$, account$]).subscribe(
       ([permissions, account]) => {
@@ -84,8 +95,9 @@ export class OrderLineComponent implements OnInit {
     this._acqOrderApiService.deleteOrderLine(this.orderLine);
   }
 
-  /** Get the color to use for the bullet
-   *  @param note - the note to analyze
+  /**
+   * Get the color to use for the bullet
+   * @param note - the note to analyze
    */
   getNoteColor(note: AcqNote): string {
     switch (note.type) {
