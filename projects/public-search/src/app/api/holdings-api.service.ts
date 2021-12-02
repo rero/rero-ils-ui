f8ddb92c26eee052d89b5a325de52a97d430f42d
Valeria Granata <valeria.granata@rero.ch>
2021-12-02 18:04:28 +0100

    holding request: add request for holding
    Co-Authored-by: Valeria Granata <valeria@chaw.com>

--- projects/public-search/src/app/api/holdings-api.service.ts
+++ projects/public-search/src/app/api/holdings-api.service.ts
@@ -14,12 +14,15 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Record, RecordService } from '@rero/ng-core';
 import { BaseApi } from '@rero/shared';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { QueryResponse } from '../record';
+import { HoldingCanRequest, HoldingPatronRequest } from '../classes/holdings';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -28,8 +31,12 @@ export class HoldingsApiService extends BaseApi {
   /**
    * Constructor
    * @param _recordService - RecordService
+   * @param _httpClient - HttpClient
    */
-  constructor(private _recordService: RecordService) {
+  constructor(
+    private _recordService: RecordService,
+    private _httpClient: HttpClient
+  ) {
     super();
   }
 
@@ -47,4 +54,30 @@ export class HoldingsApiService extends BaseApi {
     .getRecords('holdings', query, page, itemsPerPage, undefined, { view: viewcode }, BaseApi.reroJsonheaders)
     .pipe(map((response: Record) => response.hits));
   }
+
+  /**
+   * Holding Can request
+   * @param holdingPid - Holding pid
+   * @param libraryPid - Library pid
+   * @param patronBarcode - Patron Barcode
+   * @return Observable - Check holding can be requested
+   */
+  canRequest(holdingPid: string, libraryPid: string, patronBarcode: string): Observable<HoldingCanRequest> {
+    const url = `/api/holding/${holdingPid}/can_request`;
+    const params = new HttpParams()
+      .set('library_pid', libraryPid)
+      .set('patron_barcode', patronBarcode);
+    return this._httpClient.get<HoldingCanRequest>(url, { params });
+  }
+
+  /**
+   * Holding patron request
+   * @param data - Holding pid, pickup location pid, description
+   * @return Observable - Create provisional item
+   */
+  request(data: { holding_pid: string, pickup_location_pid: string, description: string }): Observable<HoldingPatronRequest> {
+    const url = '/api/holding/patron_request';
+    return this._httpClient.post<HoldingPatronRequest>(url, data);
+  }
+
 }
