11a1987d4093c1327bda9a358d2d5629b9781b87
Renaud Michotte <renaud.michotte@gmail.com>
2022-04-13 15:56:04 +0200

    transactions: better transactions history display.
    Improves the item transaction history display to highligh transactions
    links (all transactions related to the same loan) and improves look and
    feel.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/record/circulation-logs/circulation-log/circulation-log.component.ts
+++ projects/admin/src/app/record/circulation-logs/circulation-log/circulation-log.component.ts
@@ -15,22 +15,50 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UserService } from '@rero/shared';
 
 @Component({
   selector: 'admin-circulation-log',
   templateUrl: './circulation-log.component.html',
-  styleUrls: ['./circulation-log.component.css']
+  styleUrls: ['./circulation-log.component.scss']
 })
 export class CirculationLogComponent {
+
+  // COMPONENT ATTRIBUTES =====================================================
   /** Operation log record */
   @Input() record: any;
+  /** Is the log should be highligthed */
+  @Input() isHighlight = false;
+  /** Is the transaction must be separated from sibling elements */
+  @Input() separator = false;
 
   /** Event for close dialog */
   @Output() closeDialogEvent  = new EventEmitter();
 
   /** Circulation informations is collapsed */
   isCollapsed = true;
+  /** debugMode */
+  debugMode = false;
 
+  // GETTER & SETTER ==========================================================
+  /**
+   * Is the debug mode could be activated ?
+   * @returns True if the debug mode can be enabled and switched
+   */
+  get canUseDebugMode(): boolean {
+    return this._userService.user.isSystemLibrarian;
+  }
+
+  // CONSTRUCTOR & HOOKS ======================================================
+  /**
+   * Constructor
+   * @param _userService - UserService
+   */
+  constructor(
+    private _userService: UserService
+  ) {  }
+
+  // COMPONENT FUNCTIONS ======================================================
   /** Close dialog */
   closeDialog(): void {
     this.closeDialogEvent.emit(null);
--- projects/admin/src/app/record/circulation-logs/circulation-logs.component.ts
+++ projects/admin/src/app/record/circulation-logs/circulation-logs.component.ts
@@ -17,6 +17,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Record } from '@rero/ng-core';
+import moment from 'moment';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { OperationLogsApiService } from '../../api/operation-logs-api.service';
@@ -25,34 +26,33 @@ import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
 @Component({
   selector: 'admin-circulation-logs',
   templateUrl: './circulation-logs.component.html',
-  styleUrls: ['./circulation-logs.component.css']
+  styleUrls: ['./circulation-logs.component.scss']
 })
 export class CirculationLogsComponent implements OnInit {
 
+  // COMPONENT ATTRIBUTES =====================================================
   /** Resource pid */
   @Input() resourcePid: string;
 
   /** Current page */
   page = 1;
-
   /** items per pages */
   itemsPerPage = 10;
-
   /** Total of records */
   recordTotals = 0;
-
+  /** Current highligthed loan */
+  highligthedLoanPid: string = null;
   /** Array of records */
   records = [];
-
   /** first loaded record */
   loadedRecord = false;
-
   /** Event on dialog close */
   dialogClose$ = new BehaviorSubject(false);
 
+  // GETTER & SETTER ==========================================================
   /**
-   * Show more link is visible
-   * @return boolean
+   * Is the "show more" link should be visible
+   * @return True if the link must be visible, False otherwise
    */
   get isLinkShowMore(): boolean {
     return this.recordTotals > 0
@@ -60,18 +60,19 @@ export class CirculationLogsComponent implements OnInit {
   }
 
   /**
-   * Hidden holdings count
-   * @return string
+   * Get the label to use for the hidden transaction logs string.
+   * @return the translated string to display
    */
-  get hiddenOperationLogs(): string {
+  get hiddenTransactionLabel(): string {
     let count = this.recordTotals - (this.page * this.itemsPerPage);
-    if (count < 0) { count = 0; }
+    count = (count < 0) ? 0 : count;
     const linkText = (count > 1)
       ? _('{{ counter }} hidden circulations logs')
       : _('{{ counter }} hidden circulations log');
     return this._translateService.instant(linkText, { counter: count });
   }
 
+  // CONSTRUCTOR & HOOKS ======================================================
   /**
    * Constructor
    * @param _operationLogsApiService - OperationLogsApiService
@@ -91,6 +92,7 @@ export class CirculationLogsComponent implements OnInit {
     });
   }
 
+  // PUBLIC FUNCTIONS =========================================================
   /** Close operation log dialog */
   closeDialog(): void {
     this.dialogClose$.next(true);
@@ -106,12 +108,44 @@ export class CirculationLogsComponent implements OnInit {
   }
 
   /**
+   * This function will check if two date are in the same time period
+   * @param date1: the first date to check.
+   * @param date2: the second date to check.
+   * @param period: the time period to use to compare both dates
+   * @return True if dates are similar, False otherwise
+   */
+  isSamePeriod(date1: any, date2: any, period: string = 'month'): boolean {
+    if (date1 && date2) {
+      const transDate1 = moment(date1.metadata.date);
+      const transDate2 = moment(date2.metadata.date);
+      return transDate1.isSame(transDate2, period);
+    }
+    return true;
+  }
+
+  /**
+   * Handler for mouse event
+   * @param record: the record on which the event is triggered.
+   * @param event: the triggered event.
+   */
+  mouseEventTransaction(record: any, event: Event): void {
+    if (record.metadata
+        && record.metadata.loan
+        && record.metadata.loan.pid
+        && record.metadata.loan.pid !== this.highligthedLoanPid) {
+      this.highligthedLoanPid = record.metadata.loan.pid;
+    }
+  }
+
+  // PRIVATE FUNCTIONS ========================================================
+  /**
    * Circulation logs query
    * @param page - number
-   * @returns Obserable
+   * @return Observable
    */
   private _circulationLogsQuery(page: number): Observable<any> {
-    return this._operationLogsApiService.getCirculationLogs(this.resourcePid, page, this.itemsPerPage)
-    .pipe(map((response: Record) =>  response.hits));
+    return this._operationLogsApiService
+      .getCirculationLogs(this.resourcePid, page, this.itemsPerPage)
+      .pipe(map((response: Record) =>  response.hits));
   }
 }
