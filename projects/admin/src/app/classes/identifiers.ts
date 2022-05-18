99aeed0d235ae6f975b5f71eb4ba5e1de2799c0f
Renaud Michotte <renaud.michotte@gmail.com>
2022-05-18 15:58:52 +0200

    document: `identifiedBy` duplicate check.
    When encoding a identifier for a document, check if another document
    already hold this identifier (or an alternative to this identifier).
    Closes rero/rero-ils#1893.

---
+++ projects/admin/src/app/classes/identifiers.ts
@@ -0,0 +1,44 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2019 RERO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* tslint:disable */
+// required as json properties is not lowerCamelCase
+
+export enum IdentifierTypes {
+  AUDIO_ISSUE_NUMBER = 'bf:AudioIssueNumber',
+  DOI = 'bf:Doi',
+  EAN = 'bf:Ean',
+  GTIN_14 = 'bf:Gtin14Number',
+  IDENTIFIER = 'bf:Identifier',
+  ISAN = 'bf:Isan',
+  ISBN = 'bf:Isbn',
+  ISMN = 'bf:Ismn',
+  ISRC = 'bf:Isrc',
+  ISSN = 'bf:Issn',
+  L_ISSN = 'bf:IssnL',
+  LCCN = 'bf:Lccn',
+  LOCAL = 'bf:Local',
+  MATRIX_NUMBER = 'bf:MatrixNumber',
+  MUSIC_DISTRIBUTOR_NUMBER = 'bf:MusicDistributorNumber',
+  MUSIC_PLATE = 'bf:MusicPlate',
+  MUSIC_PUBLISHER_NUMBER = 'bf:MusicPublisherNumber',
+  PUBLISHER_NUMBER = 'bf:PublisherNumber',
+  UPC = 'bf:Upc',
+  URN = 'bf:Urn',
+  VIDEO_RECORDING_NUMBER = 'bf:VideoRecordingNumber',
+  URI = 'uri'
+}
