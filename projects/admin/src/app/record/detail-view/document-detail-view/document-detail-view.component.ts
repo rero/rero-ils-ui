3d67f927e0881ce99187680a5717176075c709cd
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2022-09-29 16:02:27 +0200

    document: improve import control
    If no identifier has been selected for the control,
    we try to search by the main title
    
    * Closes rero/rero-ils#3031.
    
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/admin/src/app/record/detail-view/document-detail-view/document-detail-view.component.ts
+++ projects/admin/src/app/record/detail-view/document-detail-view/document-detail-view.component.ts
@@ -1,6 +1,6 @@
 /*
  * RERO ILS UI
- * Copyright (C) 2019 RERO
+ * Copyright (C) 2019-2022 RERO
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -22,9 +22,10 @@ import { Record, RecordService } from '@rero/ng-core';
 import { DetailRecord } from '@rero/ng-core/lib/record/detail/view/detail-record';
 import { cloneDeep } from 'lodash-es';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { forkJoin, Observable, of, Subscription } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { DocumentApiService } from '../../../api/document-api.service';
+import { IdentifierTypes } from '../../../classes/identifiers';
 import { OperationLogsService } from '../../../service/operation-logs.service';
 import { DialogImportComponent } from './dialog-import/dialog-import.component';
 
@@ -57,12 +58,22 @@ export class DocumentDetailViewComponent implements DetailRecord, OnInit, OnDest
   ddCssClass = 'col-sm-6 col-md-8 mb-0';
 
   /** Mapping types for import */
-  private mappingtypes = {
+  private _mappingtypes = {
     'bf:Ean': 'bf:Isbn',
     'bf:Isbn': 'bf:Isbn',
     'bf:Issn': 'bf:IssnL',
     'bf:IssnL': 'bf:Issn'
   };
+
+  /** Identifiers used to check for duplicates */
+  private _identifiersList = [
+    IdentifierTypes.ISBN,
+    IdentifierTypes.ISSN,
+    IdentifierTypes.DOI,
+    IdentifierTypes.LCCN,
+    IdentifierTypes.L_ISSN,
+    IdentifierTypes.EAN
+  ];
 
   /**
    * Is operation log enabled
@@ -70,96 +81,6 @@ export class DocumentDetailViewComponent implements DetailRecord, OnInit, OnDest
    */
   get isEnabledOperationLog(): boolean {
     return this._operationLogsService.isLogVisible('documents');
-  }
-
-  /**
-   * Constructor
-   * @param _translateService - TranslateService to translate some strings.
-   * @param _activatedRouter - ActivatedRoute to get url parameters.
-   * @param _recordService - RecordService to the MARC version for the record.
-   * @param _operationLogsService - OperationLogsService
-   * @param _router - Router
-   * @param _bsModalService - BsModalService
-   * @param _documentApiService - DocumentApiService
-   */
-  constructor(
-    private _translateService: TranslateService,
-    private _activatedRouter: ActivatedRoute,
-    private _recordService: RecordService,
-    private _operationLogsService: OperationLogsService,
-    private _router: Router,
-    private _bsModalService: BsModalService,
-    private _documentApiService: DocumentApiService
-  ) { }
-
-  /** On init hook */
-  ngOnInit() {
-    this._recordObs = this.record$.pipe(
-      switchMap((record: any) => {
-        this.record = record;
-        if (record != null && record.metadata != null && this.record.metadata.pid == null) {
-          this.marc$ = this._recordService.getRecord(
-            this._activatedRouter.snapshot.params.type, this.pid, 0, {
-            Accept: 'application/marc+json, application/json'
-          });
-        } else {
-          this.marc$ = of(null);
-        }
-        return this.pid
-          ? this._documentApiService.getLinkedDocumentsCount(this.pid)
-          : of(0);
-      })
-    ).subscribe((count: number) => {
-      this.linkedDocumentsCount = count;
-    });
-  }
-
-  /**
-   * Import Document
-   * @param event - Event
-   * @param record - Current record
-   * @param data - Import source reference
-   */
-  importDocument(event: Event, record: any, data: { source: string, pid: string }): void {
-    event.preventDefault();
-    const keysTypes = Object.keys(this.mappingtypes);
-    const rec = record.metadata;
-    const route = ['/records', 'documents', 'new'];
-    if (rec.identifiedBy) {
-      const queryParams = [];
-      rec.identifiedBy.forEach((identifier: any) => {
-        if (['bf:Isbn', 'bf:Issn', 'bf:Doi', 'bf:Lccn', 'bf:IssnL', 'bf:Ean'].includes(identifier.type)) {
-          queryParams.push(this._extractAndFormatQueryParams(identifier));
-          if (keysTypes.indexOf(identifier.type) > -1) {
-            const cidentifier = cloneDeep(identifier);
-            cidentifier.type = this.mappingtypes[cidentifier.type];
-            queryParams.push(this._extractAndFormatQueryParams(cidentifier));
-          }
-      }
-      });
-      const query = queryParams.join(' OR ');
-      this._recordService.getRecords(
-        'documents', query, 1, undefined, undefined, undefined, { accept: 'application/rero+json' }
-      ).subscribe((response: Record) => {
-        if (this._recordService.totalHits(response.hits.total) === 0) {
-          this._router.navigate(route, { queryParams: data });
-        } else {
-          const config = {
-            initialState: {
-              records: response.hits.hits
-            }
-          };
-          const bsModalRef = this._bsModalService.show(DialogImportComponent, config);
-          bsModalRef.content.confirmation$.subscribe((confirmation: boolean) => {
-            if (confirmation) {
-              this._router.navigate(route, { queryParams: data });
-            }
-          });
-        }
-      });
-    } else {
-      this._router.navigate(route, { queryParams: data });
-    }
   }
 
   /** Source for imported record. */
@@ -171,33 +92,11 @@ export class DocumentDetailViewComponent implements DetailRecord, OnInit, OnDest
   }
 
   /** External identifier for imported record. */
-  get pid() {
+  get pid(): string | null {
     if (this._activatedRouter.snapshot && this._activatedRouter.snapshot.params && this._activatedRouter.snapshot.params.pid !== null) {
       return this._activatedRouter.snapshot.params.pid;
     }
     return null;
-  }
-
-  /** On destroy hook */
-  ngOnDestroy(): void {
-    this._recordObs.unsubscribe();
-  }
-
-  /**
-   * Get Current language interface
-   * @return string - language
-   */
-  get currentLanguage() {
-    return this._translateService.currentLang;
-  }
-
-  /**
-   * Allow to filter provisionActivity keeping only activities that are 'Publication'
-   * @param element: the element to check
-   * @return True if element is a 'Publication', False otherwise
-   */
-  filterPublicationProvisionActivity(element: any): boolean {
-    return ('key' in element && element.key === 'bf:Publication');
   }
 
   /**
@@ -221,6 +120,137 @@ export class DocumentDetailViewComponent implements DetailRecord, OnInit, OnDest
       return this.record.metadata.electronicLocator.filter(
         (electronicLocator: any) => ['resource', 'versionOfResource'].some(t => t === electronicLocator.type)
       );
+    }
+  }
+
+  /**
+   * Get Current language interface
+   * @return string - language
+   */
+  get currentLanguage(): string {
+    return this._translateService.currentLang;
+  }
+
+  /**
+   * Constructor
+   * @param _translateService - TranslateService to translate some strings.
+   * @param _activatedRouter - ActivatedRoute to get url parameters.
+   * @param _recordService - RecordService to the MARC version for the record.
+   * @param _operationLogsService - OperationLogsService
+   * @param _router - Router
+   * @param _bsModalService - BsModalService
+   * @param _documentApiService - DocumentApiService
+   */
+  constructor(
+    private _translateService: TranslateService,
+    private _activatedRouter: ActivatedRoute,
+    private _recordService: RecordService,
+    private _operationLogsService: OperationLogsService,
+    private _router: Router,
+    private _bsModalService: BsModalService,
+    private _documentApiService: DocumentApiService
+  ) { }
+
+  /** On init hook */
+  ngOnInit(): void {
+    this._recordObs = this.record$.pipe(
+      switchMap((record: any) => {
+        this.record = record;
+        if (record != null && record.metadata != null && this.record.metadata.pid == null) {
+          this.marc$ = this._recordService.getRecord(
+            this._activatedRouter.snapshot.params.type, this.pid, 0, {
+            Accept: 'application/marc+json, application/json'
+          });
+        } else {
+          this.marc$ = of(null);
+        }
+        return this.pid
+          ? this._documentApiService.getLinkedDocumentsCount(this.pid)
+          : of(0);
+      })
+    ).subscribe((count: number) => {
+      this.linkedDocumentsCount = count;
+    });
+  }
+
+  /** On destroy hook */
+  ngOnDestroy(): void {
+    this._recordObs.unsubscribe();
+  }
+
+  /**
+   * Allow to filter provisionActivity keeping only activities that are 'Publication'
+   * @param element: the element to check
+   * @return True if element is a 'Publication', False otherwise
+   */
+  filterPublicationProvisionActivity(element: any): boolean {
+    return ('key' in element && element.key === 'bf:Publication');
+  }
+
+  /**
+   * Import Document
+   * @param event - Event
+   * @param record - Current record
+   * @param data - Import source reference
+   */
+  importDocument(event: Event, record: any, data: { source: string, pid: string }): void {
+    event.preventDefault();
+    const rec = record.metadata;
+    const route = ['/records', 'documents', 'new'];
+    const queryParams = [];
+    // If we have identifiers
+    if (rec.identifiedBy) {
+      // We select only a defined part of the identifier
+      rec.identifiedBy
+        .filter((identifier: any) => this._identifiersList.includes(identifier.type))
+        .map((identifier: any) => {
+          queryParams.push(this._extractAndFormatQueryParams(identifier));
+          if (identifier.type in this._mappingtypes) {
+            const cidentifier = cloneDeep(identifier);
+            cidentifier.type = this._mappingtypes[cidentifier.type];
+            queryParams.push(this._extractAndFormatQueryParams(cidentifier));
+          }
+        });
+    }
+
+    // If we do not have a identifier and we have a title
+    // TODO: Nice to have -  create an backend API entrypoint to detect the duplicate resource.
+    // https://github.com/rero/rero-ils/issues/2900 (Generalize to all resources)
+    if (queryParams.length === 0 && rec.title) {
+      // We extract the title
+      const titles = rec.title.filter((rtitle: any) => rtitle.type === 'bf:Title' && '_text' in rtitle);
+      if (titles.length > 0) {
+        // We clean the text string by deleting some characters
+        const regex = /["\[\]]/gi;
+        queryParams.push(`title._text:"${titles[0]._text.replace(regex, '')}"`);
+      }
+    }
+
+    // No identifier and no title, we redirect to the add form.
+    if (queryParams.length === 0) {
+      this._router.navigate(route, { queryParams: data });
+    } else {
+      // Find documents(s) with query params
+      const query = queryParams.join(' OR ');
+      this._recordService.getRecords(
+        'documents', query, 1, undefined, undefined, undefined, { accept: 'application/rero+json' }
+      ).subscribe((response: Record) => {
+        if (this._recordService.totalHits(response.hits.total) === 0) {
+          this._router.navigate(route, { queryParams: data });
+        } else {
+          const config = {
+            initialState: {
+              records: response.hits.hits
+            }
+          };
+          const bsModalRef = this._bsModalService.show(DialogImportComponent, config);
+          bsModalRef.content.confirmation$.subscribe((confirmation: boolean) => {
+            if (confirmation) {
+              this._router.navigate(route, { queryParams: data });
+            }
+          });
+        }
+      });
     }
   }
 
@@ -258,15 +288,20 @@ export class DocumentDetailViewComponent implements DetailRecord, OnInit, OnDest
   getPartOfLabel(hostDocument: any) {
     switch (hostDocument.metadata.issuance.subtype) {
       case 'periodical':
-          return this._translateService.instant('Journal');
+        return this._translateService.instant('Journal');
       case 'monographicSeries':
-          return this._translateService.instant('Series');
+        return this._translateService.instant('Series');
       default:
-           return this._translateService.instant('Published in');
+        return this._translateService.instant('Published in');
     }
   }
 
-  contributionTypeParam(contribution: any) {
+  /**
+   * Translate the type of the contribution for url
+   * @param contribution - document contribution
+   * @returns string, the translated type
+   */
+  contributionTypeParam(contribution: any): string {
     switch (contribution.type) {
       case 'bf:Person':
         return 'persons';
@@ -282,14 +317,11 @@ export class DocumentDetailViewComponent implements DetailRecord, OnInit, OnDest
    * @param identifier - IdentifiedBy object
    * @return string, query formatted
    */
-   private _extractAndFormatQueryParams(identifier: any): string {
+  private _extractAndFormatQueryParams(identifier: any): string {
     const query = [];
-    /* const type = identifier.type.replace(/:/g, '\\:');
-    const value = identifier.value.replace(/:/g, '\\:'); */
     query.push(`identifiedBy.type:"${identifier.type}"`);
     query.push(`identifiedBy.value:"${identifier.value}"`);
     if (identifier.source) {
-      /* const source = identifier.source.replace(/:/g, '\\:'); */
       query.push(`identifiedBy.source:"${identifier.source}"`);
     }
     return `(${query.join(' AND ')})`;
