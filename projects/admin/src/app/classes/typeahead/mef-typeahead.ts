081b8a1ccde2b1044a2ccaba8785f74b9f8ae71f
Renaud Michotte <renaud.michotte@gmail.com>
2023-06-23 10:08:17 +0200

    typeahead: custom remote entity typeahead component
    Creates a remote typeahead component that allows to split suggestions
    into several column.
    
    Co-Authored-By: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/classes/typeahead/mef-typeahead.ts
+++ projects/admin/src/app/classes/typeahead/mef-typeahead.ts
@@ -1,7 +1,7 @@
 /*
  * RERO ILS UI
- * Copyright (C) 2020-2023 RERO
- * Copyright (C) 2020-2023 UCLouvain
+ * Copyright (C) 2019-2023 RERO
+ * Copyright (C) 2019-2023 UCLouvain
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -19,46 +19,76 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { SuggestionMetadata } from '@rero/ng-core';
+import { ApiService, RecordService, SuggestionMetadata } from '@rero/ng-core';
 import { AppSettingsService } from '@rero/shared';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { ITypeahead } from './ITypeahead-interface';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MefTypeahead implements ITypeahead {
 
-  /** Entry point for MEF Api */
-  private _apiSearchEntryPoint = 'api/entities/remote/search';
+  /** Entry point API to get remote entity suggestions. */
+  private _remoteSearchEntrypoint = 'api/remote_entities/search';
+  /** Proxy API to use to get content of remote entity metadata (to avoid CORS javascript errors). */
   private _apiProxyEntryPoint = 'api/proxy';
+  /** Entry point API to get locale entity suggestions. */
+  private _localSearchEntrypoint = 'api/local_entities/search';
 
   /**
    * Constructor
    * @param _http - HttpClient
    * @param _translateService - TranslateService
    * @param _appSettingsService - AppSettingsService
+   * @param _recordService - RecordService
+   * @param _apiService - ApiService
    */
   constructor(
     protected _http: HttpClient,
     protected _translateService: TranslateService,
-    protected _appSettingsService: AppSettingsService
-  ) { }
+    protected _appSettingsService: AppSettingsService,
+    private _recordService: RecordService,
+    private _apiService: ApiService
+  ) {  }
 
   /** Get name of typeahead */
   getName() {
     return 'mef';
   }
 
+  // GET HTML REPRESENTATION ==================================================
   /**
    * Convert the input value (i.e. $ref url) into a template html code.
-   * @param options - remote typeahead options
-   * @param value - formControl value i.e. $ref value
-   * @returns Observable of string - html template representation of the value.
+   *
+   * The value could represent either a remote entity, either a local entity.
+   * A remote entity is an entity where metadata are stored outside the RERO
+   * application system. A local entity is store as a Invenio resource into the
+   * RERO application system.
+   *
+   * @param options: typeahead options
+   * @param value: formControl value i.e. $ref value
+   * @returns Observable on HTML template representation of the value.
    */
   getValueAsHTML(options: any, value: string): Observable<string> {
-    // We need to call the url to get remote metadata. But we can't do that directly because this url could be external.
-    // So we need to call a proxy passing the URL as query string argument.
+    // Determine if value is a local or remote entity ; depending on the
+    // entity type the metadata retrieve method isn't the same
+    return (value.match(/local_entities\/.*$/))
+       ? this._getLocalValueAsHTML(options, value)
+       : this._getRemoteValueAsHTML(options, value);
+  }
+
+  /**
+   * Get HTML representation for a remote entity value.
+   *
+   * @param options: typeahead options
+   * @param value: formControl value i.e. $ref value
+   * @returns Observable on HTML template representation of the value.
+   */
+  private _getRemoteValueAsHTML(options: any, value: string): Observable<string> {
+    // We need to call the url to get remote metadata. But we can't do that directly because this url could be external. So we need to call
+    // a proxy passing the URL as query string argument.
     const source = value.split('/').slice(-2)[0];
     const params = new HttpParams().set('url', value);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
@@ -81,20 +111,75 @@ export class MefTypeahead implements ITypeahead {
           return data;
         }),
         map((data: any) => {
-          const link = this._get_source_uri(data?.metadata?.identifiedBy, source) || value;
-          const label = data?.metadata?.authorized_access_point;
-          return `
-            <a href="${link}" target="_blank">${label} <i class="fa fa-external-link"></i></a>
-            <small class="badge badge-secondary ml-1">${source.toUpperCase()}</small>
-          `
+          const entity = {
+            uri: this._get_source_uri(data?.metadata?.identifiedBy, source) || value,
+            label: data?.metadata?.authorized_access_point
+          };
+          const badges = [{label: source.toUpperCase()}];
+          return this._buildEntityResolutionResponse(entity, badges);
         }),
-        catchError(e => {
-          console.error(`getValueAsHTML :: Unable to resolve ${value}`, e);
-          return of(`<span class="text-warning"><i class="fa fa-exclamation-triangle mr-1"></i>${value}</span>`);
-        })
+        catchError(e => this._handleEntityResolutionError(e, value))
     );
   }
 
+  /**
+   * Get HTML representation for a local entity value.
+   *
+   * @param options: typeahead options
+   * @param value: formControl value i.e. $ref value
+   * @returns Observable on HTML template representation of the value.
+   */
+  private _getLocalValueAsHTML(options: any, value: string): Observable<string> {
+    const pid = value.split('/').slice(-1)[0];
+    return this._recordService
+      .getRecord('local_entities', pid)
+      .pipe(
+         map((data: any) => {
+           const entity = {
+             label: data.metadata.authorized_access_point,
+             uri:  this._buildLocalEntityDetailViewURI(data.metadata.pid)
+           };
+           const badges = [];
+           if (data.metadata?.source_catalog) {
+             badges.push({label: data.metadata.source_catalog});
+           }
+           return this._buildEntityResolutionResponse(entity, badges);
+         }),
+         catchError(e => this._handleEntityResolutionError(e, value))
+      );
+  }
+
+  /**
+   * Build the HTML representation for an entity.
+   * @param entity: the entity to represent.
+   * @param badges: badges list to append to the entity.
+   * @returns the HTML representation for the entity.
+   */
+  private _buildEntityResolutionResponse(entity: {label:string, uri?: string}, badges: Array<{label: string, type?: string}>): string {
+    let output = (entity?.uri)
+      ? `<a href="${entity.uri}" target="_blank">${entity.label} <i class="fa fa-external-link"></i></a>`
+      : `<span>${entity.label}</span>`;
+    badges.forEach(badge => {
+      const className = (badge?.type)
+        ? `badge-${badge.type}`
+        : 'badge-secondary';
+      output += `<small class="badge ${className} ml-1">${badge.label}</small>`;
+    });
+    return output;
+  }
+
+  /**
+   * Manage entity resolution error.
+   * @param error: the triggered error
+   * @param value: the entity value causing the resolution error.
+   */
+  private _handleEntityResolutionError(error: Error, value: string): Observable<string> {
+    console.error(`getValueAsHTML :: Unable to resolve entity :: ${value}`, error);
+    return of(`<span class="text-warning"><i class="fa fa-exclamation-triangle mr-1"></i>${value}</span>`);
+  }
+
+
+  // GET ENTITY SUGGESTIONS ===================================================
   /**
    * Get the suggestions list given a search query.
    * @param options - remote typeahead options
@@ -111,34 +196,54 @@ export class MefTypeahead implements ITypeahead {
     if (null == options?.filters?.selected) {
       throw Error('Missing filters definition');
     }
-
     const searchCategory = options.filters.selected;
-    // TODO :: Why do I need to add an ending slash to use proxy-config and don't be redirected (308) receiving a CORS errors
-    const url = `${this._apiSearchEntryPoint}/${searchCategory}/${searchTerm}/`;
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this._http.get(url, {headers}).pipe(
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const params = new HttpParams().set('size', numberOfSuggestions);
+    // Build API call to get remote suggestions.
+    const remoteUrl = `${this._remoteSearchEntrypoint}/${searchCategory}/${searchTerm}/`;
+    const remoteSuggestions$ = this._http.get(remoteUrl, {headers, params}).pipe(
       map((results: any) => results?.hits?.total >= 0 ? results.hits.hits : []),
-      map((hits: Array<any>) => {
-        const suggestions = [];
-        hits.map((hit: any) => {
-          for (const source of this._sources()) {
-            if (hit.metadata[source]) {
-              suggestions.push(this._getNameRef(hit.metadata, source));
-            }
-          }
-        });
-        return suggestions;
-      }),
+      map((hits: Array<any>) => this._buildRemoteSuggestions(hits)),
       catchError(e => {
         switch (e.status) {
-          case 400:
-            return of([]);
-          default:
-            throw e;
+          case 400: return of([]);
+          default: throw e;
         }
       })
     );
+    // Build API call to get local entity suggestions.
+    const localUrl = `${this._localSearchEntrypoint}/${searchCategory}/${searchTerm}/`;
+    const localSuggestions$ = this._http.get(localUrl, {headers, params}).pipe(
+      map((hits: Array<any>) => this._buildLocalSuggestions(hits)),
+      catchError(e => {
+        switch (e.status) {
+          case 400: return of([]);
+          default: throw e;
+        }
+      })
+    );
+    // Return the concatenation of both API request.
+    return forkJoin([remoteSuggestions$, localSuggestions$]).pipe(
+      map(([remoteHits, localHits]) => [...remoteHits, ...localHits])
+    );
+  }
+
+  /**
+   * Build suggestions for remote entities proxy response.
+   * @param hits: the hits returned by the API call.
+   * @returns Suggestions corresponding to hits.
+   */
+  private _buildRemoteSuggestions(hits: any): SuggestionMetadata[] {
+    const suggestions = [];
+    hits.map((hit: any) => {
+      for (const source of this._sources()) {
+        if (hit.metadata[source]) {
+          suggestions.push(this._getNameRef(hit.metadata, source));
+        }
+      }
+    });
+    return suggestions;
   }
 
   /**
@@ -154,7 +259,8 @@ export class MefTypeahead implements ITypeahead {
       label: metadata.authorized_access_point,
       externalLink: this._get_source_uri(metadata?.identifiedBy, sourceName),
       value: this._get_source_uri(metadata?.identifiedBy, 'mef'),
-      group: this._translateService.instant('link to authority {{ sourceName }}', { sourceName })
+      group: this._translateService.instant('link to authority {{ sourceName }}', {sourceName}),
+      column: 0
     };
   }
 
@@ -184,5 +290,33 @@ export class MefTypeahead implements ITypeahead {
       : order[key];
     const sources = agentSources.filter((source: string) => source !== 'rero');
     return sources;
+  }
+
+  /**
+   * Build suggestions for local entities API call.
+   * @param hits: the hits returned by the API call.
+   * @returns Suggestions corresponding to hits.
+   */
+  private _buildLocalSuggestions(hits: any): SuggestionMetadata[] {
+    return hits.map(hit => {
+      return {
+        label: hit.authorized_access_point,
+        description: hit?.source_catalog,
+        value: this._apiService.getRefEndpoint('local_entities', hit.pid),
+        externalLink: this._buildLocalEntityDetailViewURI(hit.pid),
+        group: this._translateService.instant('link to local authority'),
+        column: 1,
+      };
+    });
+  }
+
+  /**
+   * Creates a local entity detail view URI
+   * TODO :: find a better/smarter way to build this URI than current static string construction..
+   * @param pid - the entity pid
+   * @returns the entity detail view URI
+   */
+  private _buildLocalEntityDetailViewURI(pid: string): string {
+    return `/records/local_entities/detail/${pid}`;
   }
 }
