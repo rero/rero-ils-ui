275e8a6a1c609a153a10828263bff92e5dbe793d
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2023-06-28 15:38:25 +0200

    entities: professional detail view
    * Adds views for local entities
    * Updates views for remote entities.
    * Deletes document list.
    * Adds document search with a button.
    * Deletes some test files do not define tests.
    
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/admin/src/app/record/detail-view/entities-detail-view/local/local-place-detail-view/local-place-detail-view.component.ts
+++ projects/admin/src/app/record/detail-view/entities-detail-view/local/local-place-detail-view/local-place-detail-view.component.ts
@@ -1,6 +1,7 @@
 /*
  * RERO ILS UI
- * Copyright (C) 2020 RERO
+ * Copyright (C) 2019-2023 RERO
+ * Copyright (C) 2019-2023 UCLouvain
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -14,30 +15,13 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'admin-corporate-bodies-detail-view',
-  templateUrl: './corporate-bodies-detail-view.component.html'
+  selector: 'admin-local-place-detail-view',
+  templateUrl: './local-place-detail-view.component.html',
 })
-export class CorporateBodiesDetailViewComponent {
-
-  /** Record metadata */
+export class LocalPlaceDetailViewComponent {
+  /** the current record */
   @Input() record: any;
-
-  /** Record source */
-  @Input() source: string;
-
-  /** Disabled source link */
-  disabledSourceLink = ['rero'];
-
-  /**
-   * Disabled link
-   * @param source - string
-   * @returns boolean
-   */
-  disabledLink(source: string) {
-    return !this.disabledSourceLink.includes(source);
-  }
 }
