b3ef49544dea66ac1460bcbaf19b37279c89e3db
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2023-07-18 09:02:48 +0200

    entities: autocomplete search bar
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/shared/src/lib/service/search-bar-config.service.ts
+++ projects/shared/src/lib/service/search-bar-config.service.ts
@@ -1,6 +1,7 @@
 /*
  * RERO ILS UI
- * Copyright (C) 2020 RERO
+ * Copyright (C) 2019-2023 RERO
+ * Copyright (C) 2019-2023 UCLouvain
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -20,6 +21,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { TruncateTextPipe } from '@rero/ng-core';
 import { MainTitlePipe } from '../pipe/main-title.pipe';
 import { AppSettingsService } from './app-settings.service';
+import { EntityType, EntityTypeIcon } from '../class/entities';
 
 @Injectable({
   providedIn: 'root'
@@ -30,13 +32,13 @@ export class SearchBarConfigService {
   private _viewcode: string = undefined;
 
   /** Typeahead options limit */
-  private _typeaheadOptionsLimit = 15;
+  private _typeaheadOptionsLimit: number = 15;
 
   /** Max Length Suggestion */
-  private _maxLengthSuggestion = 100;
+  private _maxLengthSuggestion: number = 100;
 
   /** Is admin */
-  private _isAdmin = false;
+  private _isAdmin: boolean = false;
 
   /** Set typeahead options limit */
   set typeaheadOptionsLimit(limit: number) {
@@ -44,7 +46,7 @@ export class SearchBarConfigService {
   }
 
   /** Get typeahead options limit */
-  get typeaheadOptionsLimit() {
+  get typeaheadOptionsLimit(): number {
     return this._typeaheadOptionsLimit;
   }
 
@@ -64,44 +66,31 @@ export class SearchBarConfigService {
 
   /**
    * Get Config
-   * @param isAdmin - boolean
+   * @param isAdmin - boolean, are we on the admin interface
    * @param component - any
-   * @param viewcode - string
-   * @param maxLengthSuggestion - number
+   * @param viewcode - string or undefined, view code for the public interface
+   * @param maxLengthSuggestion - number of max suggestion
    * @return array of contribution types configuration
    */
-  getConfig(isAdmin: boolean, component: any, viewcode?: string | undefined, maxLengthSuggestion: number = 100) {
+  getConfig(isAdmin: boolean, component: any, viewcode?: string | undefined, maxLengthSuggestion: number = 100): any[] {
     this._isAdmin = isAdmin;
     this._viewcode = viewcode;
     this._maxLengthSuggestion = maxLengthSuggestion;
 
-    // If you add a new type, please specify it in the "shared-config.service.ts"
-    // file on agentAgentTypes attribute
     return [
       {
         type: 'documents',
         field: 'autocomplete_title',
-        maxNumberOfSuggestions: 5,
-        getSuggestions: (query: any, documents: any) => this._getDocumentsSuggestions(query, documents),
+        maxNumberOfSuggestions: 10,
+        getSuggestions: (query: any, documents: any) => this.getDocumentsSuggestions(query, documents),
         preFilters: viewcode !== undefined ? { view: viewcode } : {}
       },
       {
-        type: 'persons',
-        index: 'entities',
+        type: 'entities',
         field: 'autocomplete_name',
         maxNumberOfSuggestions: 5,
-        getSuggestions: (query: any, persons: any) => this._getPersonsSuggestions(query, persons),
-        component,
-        preFilters: viewcode !== undefined ? { view: viewcode, type: 'bf:Person' } : { type: 'bf:Person' }
-      },
-      {
-        type: 'organisations',
-        index: 'entities',
-        field: 'autocomplete_name',
-        maxNumberOfSuggestions: 5,
-        getSuggestions: (query: any, organisations: any) => this._getOrganisationsSuggestions(query, organisations),
-        component,
-        preFilters: viewcode !== undefined ? { view: viewcode, type: 'bf:Organisation' } : { type: 'bf:Organisation' }
+        getSuggestions: (query: any, entities: any) => this.getEntitiesSuggestions(query, entities),
+        preFilters: viewcode !== undefined ? { view: viewcode } : {}
       }
     ];
   }
@@ -109,10 +98,10 @@ export class SearchBarConfigService {
   /**
    * Get documents suggestions
    * @param query - string, string searched
-   * @param documents - search result records
+   * @param documents - documents hits
    * @return array - array of records formatted
    */
-  private _getDocumentsSuggestions(query: string, documents: any) {
+  private getDocumentsSuggestions(query: string, documents: any): any[] {
     const values = [];
     documents.hits.hits.map((hit: any) => {
       const title = this._mainTitlePipe.transform(hit.metadata.title);
@@ -122,7 +111,7 @@ export class SearchBarConfigService {
       }
 
       values.push({
-        text: this._highlightSearch(text, query),
+        text: this.highlightSearch(text, query),
         query: title.replace(/[:\-\[\]()/"]/g, ' ').replace(/\s\s+/g, ' '),
         index: 'documents',
         category: this._translateService.instant('documents')
@@ -132,93 +121,69 @@ export class SearchBarConfigService {
   }
 
   /**
-   * Get persons suggestions
+   * Get entities suggestion
    * @param query - string, string searched
-   * @param persons - search result records
-   * @return array - array of records formatted
+   * @param entities - entities hits
+   * @returns array - array of records formatted
    */
-  private _getPersonsSuggestions(query: string, persons: any[]) {
-    return this._getContributionsSuggestions(
-      query,
-      persons,
-      'persons',
-      this._translateService.instant('person direct links'),
-      'user'
-    );
-  }
-
-  /**
-   * Get organisations suggestions
-   * @param query - string, string searched
-   * @param organisations - search result records
-   * @return array - array of records formatted
-   */
-  private _getOrganisationsSuggestions(query: string, organisations: any[]) {
-    return this._getContributionsSuggestions(
-      query,
-      organisations,
-      'corporate-bodies',
-      this._translateService.instant('corporate bodies direct links'),
-      'building'
-    );
-  }
-
-
-  /**
-   * Get contributions suggestions
-   * @param query - string, string searched
-   * @param records - search result records
-   * @param urlIndex - string, url link for current type
-   * @param category - string, title for select group
-   * @param icon - string, name of icon (without prefix fa-)
-   * @param isAdmin - boolean
-   * @return array - array of records formatted
-   */
-  private _getContributionsSuggestions(
-    query: string,
-    records: any,
-    urlIndex: string,
-    category: string,
-    icon: string
-  ) {
+  private getEntitiesSuggestions(query: string, entities: any): any[] {
     const values = [];
-    records.hits.hits.map((hit: any) => {
+    entities.hits.hits.map((hit: any) => {
       const text = this._getContributionName(hit.metadata);
       values.push({
-        text: this._highlightSearch(text, query),
-        query: '',
+        text: this.highlightSearch(text, query),
         index: 'entities',
-        category,
-        href: this._generateContributionLink(urlIndex, hit.metadata.pid),
-        iconCssClass: `fa fa-${icon}`
+        query: '',
+        category: this._translateService.instant('authors/subjects'),
+        href: this.generateEntityLink(hit.metadata),
+        iconCssClass: `fa ${this.findIconByType(hit.metadata.type)}`
       });
     });
     return values;
   }
 
   /**
-   *
+   * Highlight search term
    * @param text - text of result
    * @param query - query
    * @return string - highlight text
    */
-  private _highlightSearch(text: string, query: string) {
+  private highlightSearch(text: string, query: string): string {
     return text.replace(new RegExp(
-      this._escapeRegExp(query), 'gi'),
+      this.escapeRegExp(query), 'gi'),
       `<b>${query}</b>`
       );
   }
 
   /**
-   * Generate Contribution Link
+   * Generate Entity Link
    * @param type - string, type of contribution
    * @param pid - string, pid of record
+   * @return the url path
    */
-  private _generateContributionLink(type: string, pid: string) {
+  private generateEntityLink(metadata: any): string {
+    const { pid, resource_type } = metadata;
     if (this._isAdmin) {
-      return `/records/${type}/detail/${pid}`;
+      return `/records/${resource_type}_entities/detail/${pid}`;
     } else {
-      return `/${this._viewcode}/${type}/${pid}`;
+      return `/${this._viewcode}/entities/${resource_type}/${pid}`;
+    }
+  }
+
+  /**
+   * Find icon by type
+   * @param type - entity type (Ex: bf:Organisation)
+   * @returns The class icon
+   */
+  private findIconByType(type: string): string {
+    switch(type) {
+      case EntityType.ORGANISATION : return EntityTypeIcon.ORGANISATION;
+      case EntityType.PERSON : return EntityTypeIcon.PERSON;
+      case EntityType.PLACE : return EntityTypeIcon.PLACE;
+      case EntityType.TEMPORAL : return EntityTypeIcon.TEMPORAL;
+      case EntityType.TOPIC : return EntityTypeIcon.TOPIC;
+      case EntityType.WORK : return EntityTypeIcon.WORK;
+      default: return 'fa-question-circle';
     }
   }
 
@@ -227,7 +192,7 @@ export class SearchBarConfigService {
    * @param data - string
    * @return string
    */
-  private _escapeRegExp(data: string) {
+  private escapeRegExp(data: string): string {
     // $& means the whole matched string
     return data.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
   }
@@ -238,17 +203,23 @@ export class SearchBarConfigService {
    * @return string
    */
   private _getContributionName(metadata: any): string {
-    const language = this._translateService.currentLang;
-    const order: any = this._appSettingsService.agentLabelOrder;
-    const key = (language in order) ? language : 'fallback';
-    const agentSources = (key === 'fallback')
-      ? order[order[key]]
-      : order[key];
+    if (metadata.resource_type === 'remote') {
+      const language = this._translateService.currentLang;
+      const order: any = this._appSettingsService.agentLabelOrder;
+      const key = (language in order) ? language : 'fallback';
+      const agentSources = (key === 'fallback')
+        ? order[order[key]]
+        : order[key];
 
-    for (const source of agentSources) {
-      if (metadata[source] && metadata[source].authorized_access_point) {
-        return metadata[source].authorized_access_point;
+      for (const source of agentSources) {
+        if (metadata[source] && metadata[source].authorized_access_point) {
+          return metadata[source].authorized_access_point;
+        }
       }
+    } else {
+      const key = `authorized_access_point_${this._translateService.currentLang}`;
+      const accessPointKey = Object.keys(metadata).includes(key) ? key : 'authorized_access_point';
+      return metadata[accessPointKey];
     }
   }
 }
