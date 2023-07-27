c7d17e9cc516768435ea52d8385502dfa7a46460
Renaud Michotte <renaud.michotte@gmail.com>
2023-07-27 14:07:00 +0200

    circulation: fix item properties access on item view
    * Closes rero/rero-ils#3319.
    * If an item was in transit and is checkin in the destination library,
      the `loan` properties isn't yet present for the item. This PR
      protects access to this properties to not block UI.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- projects/admin/src/app/circulation/item/item.component.ts
+++ projects/admin/src/app/circulation/item/item.component.ts
@@ -1,6 +1,7 @@
 /*
  * RERO ILS UI
- * Copyright (C) 2019 RERO
+ * Copyright (C) 2019-2023 RERO
+ * Copyright (C) 2019-2023 UCLouvain
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
