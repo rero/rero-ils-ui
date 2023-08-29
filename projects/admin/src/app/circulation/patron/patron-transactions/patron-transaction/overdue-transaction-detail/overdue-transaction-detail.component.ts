967bbe988c8e4758284765c7d8f4757a2a89c3f6
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2023-08-29 11:56:03 +0200

    fix: display manual overdue
    * closes rero/rero-ils#3438.
    
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/admin/src/app/circulation/patron/patron-transactions/patron-transaction/overdue-transaction-detail/overdue-transaction-detail.component.ts
+++ projects/admin/src/app/circulation/patron/patron-transactions/patron-transaction/overdue-transaction-detail/overdue-transaction-detail.component.ts
@@ -1,3 +1,20 @@
+/*
+ * RERO ILS UI
+ * Copyright (C) 2019-2023 RERO
+ * Copyright (C) 2019-2023 UCLouvain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import { Component, Input, OnInit } from '@angular/core';
 import { RecordService } from '@rero/ng-core';
 import { map, mergeMap } from 'rxjs/operators';
