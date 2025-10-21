c2380931be76d379da53bfa1c59c0333839f7e1e
Nathan Python <nathan.python@students.hevs.ch>
2025-10-21 13:43:04 +0200

    refactor(circulation): remove pager
    Co-Authored-by: Nathan Python <nathan.python@hes-so.ch

--- projects/public-search/src/app/patron-profile/patron-profile-loans/patron-profile-loans.component.ts
+++ projects/public-search/src/app/patron-profile/patron-profile-loans/patron-profile-loans.component.ts
@@ -14,11 +14,9 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { _ } from "@ngx-translate/core";
+import { Component, inject, OnInit } from '@angular/core';
 import { Error, Record } from '@rero/ng-core';
-import { Paginator } from '@rero/shared';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { LoanApiService } from '../../api/loan-api.service';
 import { PatronProfileMenuService } from '../patron-profile-menu.service';
 
@@ -27,13 +25,11 @@ import { PatronProfileMenuService } from '../patron-profile-menu.service';
     templateUrl: './patron-profile-loans.component.html',
     standalone: false
 })
-export class PatronProfileLoansComponent implements OnInit, OnDestroy {
+export class PatronProfileLoansComponent implements OnInit {
 
   private loanApiService: LoanApiService = inject(LoanApiService);
   private patronProfileMenuService: PatronProfileMenuService = inject(PatronProfileMenuService);
 
-  /** Observable subscription */
-  private _subscription = new Subscription();
 
   /** First get record */
   loaded = false;
@@ -44,59 +40,9 @@ export class PatronProfileLoansComponent implements OnInit, OnDestroy {
   /** sort criteria */
   sortCriteria = 'duedate';
 
-  /** paginator page */
-  page = 1;
-
-  /** number of records per paginator page */
-  nRecords = 20;
-
-  /** Records paginator */
-  paginator: Paginator;
-
   /** OnInit hook */
   ngOnInit(): void {
-    this._initializePaginatorAndSubscription();
-    this._initialLoad();
-  }
-
-  /** OnDestroy hook */
-  ngOnDestroy(): void {
-    this._subscription.unsubscribe();
-  }
-
-  /** Initialize paginator and subscription */
-  private _initializePaginatorAndSubscription(): void {
-    this.paginator = new Paginator();
-    this.paginator
-      .setRecordsPerPage(this.nRecords)
-      .setHiddenInfo(
-        _('({{ count }} hidden loan)'),
-        _('({{ count }} hidden loans)')
-      );
-
-    this._subscription = new Subscription();
-    this._subscription.add(
-      this.paginator.more$.subscribe((page: number) => {
-        this._loanQuery(page).subscribe((response: Record) => {
-          this.records = this.records.concat(response.hits.hits);
-          this.page = page;
-        });
-      })
-    );
-    this._subscription.add(
-      this.patronProfileMenuService.onChange$.subscribe(() => {
-        this._resetPaginator();
-      })
-    );
-  }
-
-  /** Initial records load */
-  private _initialLoad(): void {
-    this._loanQuery(1).subscribe((response: Record) => {
-      this.paginator.setRecordsCount(response.hits.total.value);
-      this.records = response.hits.hits;
-      this.loaded = true;
-    });
+    this.selectingSortCriteria(this.sortCriteria);
   }
 
   /**
@@ -104,24 +50,12 @@ export class PatronProfileLoansComponent implements OnInit, OnDestroy {
    * @param page - number
    * @return Observable
    */
-  private _loanQuery(page: number): Observable<Record | Error> {
+  private _loanQuery(): Observable<Record | Error> {
     const patronPid = this.patronProfileMenuService.currentPatron.pid;
     return this.loanApiService
-      .getOnLoan(patronPid, page, this.paginator.getRecordsPerPage(), undefined, this.sortCriteria);
+      .getOnLoan(patronPid, 1, 9999, undefined, this.sortCriteria);
   }
 
-  /** Reset paginator when patron profile menu has changed */
-  private _resetPaginator(){
-    this._loanQuery(1).subscribe((response: Record) => {
-      this.paginator
-        .setPage(1)
-        .setRecordsCount(response.hits.total.value);
-
-      this.records = response.hits.hits;
-      this.page = 1;
-      this.loaded = true;
-    });
-  }
 
    /**
     * Allow to sort loans list using a sort criteria
@@ -129,11 +63,9 @@ export class PatronProfileLoansComponent implements OnInit, OnDestroy {
     */
   selectingSortCriteria(sortCriteria: string) {
     this.sortCriteria = sortCriteria;
-    this.paginator.setRecordsPerPage(this.page * this.nRecords);
-
-    this._loanQuery(1).subscribe((response: Record) => {
+   
+    this._loanQuery().subscribe((response: Record) => {
       this.records = response.hits.hits;
-      this.paginator.setRecordsPerPage(this.nRecords);
       this.loaded = true;
     });
   }
