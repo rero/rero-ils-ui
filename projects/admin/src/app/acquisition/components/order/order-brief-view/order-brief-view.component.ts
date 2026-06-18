b56f166cefde535169533a0300fa369c4147a8ef
Pascal Repond <pascal.repond@rero.ch>
2026-06-18 13:25:55 +0200

    style: replace verbose license headers with SPDX tags
    Also updates all dependencies
    
    Co-Authored-by: Pascal Repond <pascal.repond@rero.ch>

--- projects/admin/src/app/acquisition/components/order/order-brief-view/order-brief-view.component.ts
+++ projects/admin/src/app/acquisition/components/order/order-brief-view/order-brief-view.component.ts
@@ -1,20 +1,6 @@
-/*
- * RERO ILS UI
- * Copyright (C) 2021-2025 RERO
- * Copyright (C) 2021 UCLouvain
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: Fondation RERO+
+// SPDX-FileCopyrightText: UCLouvain
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { Component, input, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import { AcqNoteType } from '../../../classes/common';
